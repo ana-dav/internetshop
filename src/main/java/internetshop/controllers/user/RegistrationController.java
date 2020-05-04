ea8c0f90a4c1535c8df442125ee694083f9a7ce2
ana-dav <opa.super.op@gmail.com>
2020-05-04 12:28:52 +0300

    order/order ren

--- src/main/java/internetshop/controllers/user/RegistrationController.java
+++ src/main/java/internetshop/controllers/user/RegistrationController.java
@@ -1,47 +1,38 @@
-package internetshop.controllers.user;
+package internetshop.controllers.order;
 
 import internetshop.lib.Injector;
+import internetshop.model.Product;
 import internetshop.model.ShoppingCart;
 import internetshop.model.User;
+import internetshop.service.OrderService;
 import internetshop.service.ShoppingCartService;
-import internetshop.service.UserService;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/registration")
-public class RegistrationController extends HttpServlet {
+@WebServlet("/order")
+public class CreateOrderController extends HttpServlet {
+    private static final Long USER_ID = 1L;
+
     private static final Injector INJECTOR =
             Injector.getInstance("internetshop");
-    private final UserService userService =
-            (UserService) INJECTOR.getInstance(UserService.class);
     private final ShoppingCartService shoppingCartService =
             (ShoppingCartService) INJECTOR.getInstance(ShoppingCartService.class);
+    private final OrderService orderService =
+            (OrderService) INJECTOR.getInstance(OrderService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/registration.jsp").forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        String name = req.getParameter("name");
-        String login = req.getParameter("login");
-        String password = req.getParameter("pass");
-        String repeatPassword = req.getParameter("pass-confirm");
-        if (password.equals(repeatPassword)) {
-            User user = userService.create(new User(name, login, password));
-            ShoppingCart shoppingCart = new ShoppingCart(user);
-            shoppingCartService.create(shoppingCart);
-            resp.sendRedirect(req.getContextPath() + "/");
-        } else {
-            req.setAttribute("message", "Passwords are different");
-            req.getRequestDispatcher("WEB-INF/views/registration.jsp").forward(req, resp);
-        }
+        ShoppingCart shoppingCart = shoppingCartService.getByUserId(USER_ID);
+        User user = shoppingCart.getUser();
+        List<Product> products = List.copyOf(shoppingCart.getProducts());
+        orderService.completeOrder(products, user);
+        resp.sendRedirect(req.getContextPath() + "/orders");
+        shoppingCartService.clear(shoppingCart);
     }
 }
