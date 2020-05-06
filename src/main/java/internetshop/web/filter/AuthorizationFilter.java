fc3980134e73d170100b5021b1c7436515bcd627
ana-dav <opa.super.op@gmail.com>
2020-05-06 22:50:41 +0300

    logging

--- src/main/java/internetshop/web/filter/AuthorizationFilter.java
+++ src/main/java/internetshop/web/filter/AuthorizationFilter.java
@@ -66,7 +66,8 @@ public class AuthorizationFilter implements Filter {
         if (isAuthorized(user, protectedUrls.get(requestedUrl))) {
             chain.doFilter(req, resp);
         } else {
-            LOGGER.warn("access denied");
+            //користувач з айді = айді пробував зайти на url, до якого немає доступу
+            LOGGER.warn(" user " + user.getId() + "tries to reach" + protectedUrls.get(requestedUrl) + "url");
             req.getRequestDispatcher("/WEB-INF/views/accessDenied.jsp").forward(req, resp);
         }
     }
